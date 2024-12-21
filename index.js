3260abe841eae5076856b8684fecd74b0c4fbeb9
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-12-21 14:09:02 +0600

    file upload done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require("multer");
+const path = require("path");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -21,6 +23,43 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+// multer----------------------------
+
+
+const UPLOADS_FOLDER = "./uploads/"
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOADS_FOLDER)
+    },
+    filename: (req, file, cb) => {
+        const fileExt = path.extname(file.originalname);
+        const fileName = file.originalname.replace(fileExt, "").toLowerCase().split(" ").join("-") + "-" + Date.now();
+        cb(null, fileName + fileExt);
+    }
+})
+
+const upload = multer({
+    //dest: UPLOADS_FOLDER,
+    storage: storage,
+    limits: {
+        fileSize: 5000000 // 1mb
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+            cb(null, true)
+        }
+        else {
+            cb(new Error("Invalid file type. Only jpg and png are allowed"))
+            //cd(null, false) // to handle silently 
+        }
+    },
+})
+
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 async function run() {
     try {
         await client.connect();
@@ -29,6 +68,28 @@ async function run() {
             res.send("Setup is ok")
         })
 
+
+        app.post("/", upload.single("avatar"), (req, res) => {
+            res.send(req.file);
+        })
+
+
+
+        //error handling---------------
+        app.use((err, req, res, next) => {
+            if (err) {
+                if (err instanceof multer.MulterError) {
+                    res.status(500).send(err.message)
+                }
+                else {
+                    res.status(500).send(err.message)
+                }
+            }
+            else {
+                res.send("success")
+            }
+        })
+        //-----------------------------------
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
